708a6aeefd7f7773d9c2f13156a66320914c0b2b
tushar-neosoft <tushar.joshi@neosoftmail.com>
2025-08-19 17:50:40 +0530

    Initial Commit

---
+++ VernostTask/VernostTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  VernostTaskApp.swift
+//  VernostTask
+//
+//  Created by Apple on 19/08/25.
+//
+
+import SwiftUI
+
+@main
+struct VernostTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
